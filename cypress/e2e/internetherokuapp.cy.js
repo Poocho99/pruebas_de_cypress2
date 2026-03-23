26ba678342c0c749d8a68c2d055b4376c909334b
Rafael Lopez <rafael.lopez.serrano.99@gmail.com>
2026-03-23 21:29:23 +0100

    Pagina Internet

--- cypress/e2e/internetherokuapp.cy.js
+++ cypress/e2e/internetherokuapp.cy.js
@@ -10,7 +10,7 @@ describe('Pruebas pagina herokuapp', () => {
   
   })
   
-    it('Shifting Content Menu',() => {
+    it('Shifting content menu comprobar 5 elementos',() => {
   cy.visit('https://the-internet.herokuapp.com/')
   cy.get(':nth-child(39) > a').click()
   cy.get('[href="/shifting_content/menu"]').click()
