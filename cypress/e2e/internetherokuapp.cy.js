d71d1a12b30a6f7b0b72d5474c73b4ee1e69077b
Rafael Lopez <rafael.lopez.serrano.99@gmail.com>
2026-03-24 11:58:40 +0100

    Pagina Internet

--- cypress/e2e/internetherokuapp.cy.js
+++ cypress/e2e/internetherokuapp.cy.js
@@ -1,20 +1,20 @@
 describe('Pruebas pagina herokuapp', () => {
 
   it('Clicar Form authentication e iniciar sesion',() => {
-  cy.visit('https://the-internet.herokuapp.com/')
-  cy.get(':nth-child(21) > a').click()
-  cy.get('#username').type("tomsmith")
-  cy.get('#password').type("SuperSecretPassword!{enter}")
-  cy.get('.subheader')
-  .contains('Welcome to the Secure Area. When you are done click logout below.')
+    cy.visit('https://the-internet.herokuapp.com/')
+    cy.get(':nth-child(21) > a').click()
+    cy.get('#username').type("tomsmith")
+    cy.get('#password').type("SuperSecretPassword!{enter}")
+    cy.get('.subheader')
+    .contains('Welcome to the Secure Area. When you are done click logout below.')
   
   })
   
-    it('Shifting content menu comprobar 5 elementos',() => {
-  cy.visit('https://the-internet.herokuapp.com/')
-  cy.get(':nth-child(39) > a').click()
-  cy.get('[href="/shifting_content/menu"]').click()
-  cy.get('li').should('have.length', 5)
+  it('Shifting content menu comprobar 5 elementos',() => {
+    cy.visit('https://the-internet.herokuapp.com/')
+    cy.get(':nth-child(39) > a').click()
+    cy.get('[href="/shifting_content/menu"]').click()
+    cy.get('li').should('have.length', 5)
   
   })
 
